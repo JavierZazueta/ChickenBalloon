bc9eb3956250495aeb10d07945466bcd0ffb0661
JavierZazueta <javierzazueta23@gmail.com>
2018-06-23 17:01:03 -0600

    initial push of files

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public PlayerMove currentBalloon;//camera's target
+    //public Balloon[] balloons;
+
+    private Vector3 pos;
+    private float xOffset = 3.2f;
+    private float yOffset = 7;
+    private Vector3 newPos;
+    private Vector3 accel;
+
+    void Start()
+    {
+        //calibrateAccelerometer();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Yo");
+        }
+    }
+
+    void LateUpdate()
+    {
+        pos = currentBalloon.transform.position;
+        transform.position = new Vector3(pos.x, pos.y, -10);
+
+        accel = Input.acceleration;
+
+        newPos = new Vector3(currentBalloon.transform.position.x - (accel.x * xOffset),
+                             currentBalloon.transform.position.y - (accel.y * yOffset),
+                             transform.position.z);
+        transform.position = newPos;
+    }
+
+    //http://answers.unity3d.com/questions/927515/accelerometer-calibration-2.html
+
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private float MOVE_SPEED = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * MOVE_SPEED * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.right * -MOVE_SPEED * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * MOVE_SPEED * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.up * -MOVE_SPEED * Time.deltaTime;
+        }
+    }
+}
